aff65a64859d7612dd950ab91b9c92b9d3518ddd
ujangaripin24 <ujangarifin06@gmail.com>
2022-11-13 20:29:26 +0700

    13. pagination has been installed

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ const store = new sessionStore({
     db: db
 });
 
-(async() =>{
-    await db.sync();
-})();
+// (async() =>{
+//     await db.sync();
+// })();
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
